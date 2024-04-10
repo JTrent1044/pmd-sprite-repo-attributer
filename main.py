ce2a821e28faf952234ffa0d90852f3ae1a6732e
JT <79288490+JTrent1044@users.noreply.github.com>
2024-04-10 04:17:54 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,147 @@
+import sys, os
+from tqdm import tqdm
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException,StaleElementReferenceException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+IGNORED_EXCEPTIONS=(NoSuchElementException,StaleElementReferenceException,)
+
+#Firefox does not understand how to scroll automatically to objects we want to put the mouse cursor over.
+#Thus, we have to do it for it.
+def scroll_shim(passed_in_driver, object):
+    x = object.location['x']
+    y = object.location['y']
+    scroll_by_coord = 'window.scrollTo(%s,%s);' % (
+        x,
+        y
+    )
+    passed_in_driver.execute_script(scroll_by_coord)
+
+#Get attributions for portraits of a certain mon-form combination
+def get_portrait_attributions(driver,pokemon_name="",pokemon_form_name=""): #Don't call this on its own.
+
+    portraits_path = "//div[contains(concat(' ', @class, ' '), ' MuiContainer-root ')]/div[2]/div[1]"
+    assert driver.find_element(By.XPATH,portraits_path+"/div/div/h5").get_attribute("innerHTML") == "Portraits", "Pathing to portraits failed."
+
+    try:
+        driver.find_element(By.XPATH,portraits_path+"/h5")
+        print(f"No portraits in Repo for {pokemon_name} {pokemon_form_name}.",file=sys.stderr)
+        return ""
+    except NoSuchElementException:
+        #We don't actually need to do anything here because if this element doesn't exist, good.
+        pass
+
+    portrait_objects = driver.find_elements(By.XPATH, portraits_path+"/div[2]/div[contains(concat(' ', @class, ' '), ' MuiGrid-item ')]/div/div")
+
+    portraits = []
+    for portrait in portrait_objects:
+        portrait_name = portrait.find_element(By.XPATH, ".//p").get_attribute("innerHTML")
+        portrait_hover = portrait.find_element(By.XPATH, ".//*[name()='svg']")
+        ActionChains(driver).move_to_element(portrait_hover).perform()
+        popup = WebDriverWait(driver,10,ignored_exceptions=IGNORED_EXCEPTIONS).until(EC.presence_of_element_located((By.XPATH,"//div[contains(concat(' ', @class, ' '), ' MuiTooltip-popper ')]/div")))
+        portrait_authors = [el.get_attribute("innerHTML") for el in popup.find_elements(By.XPATH,".//div/div/a")]
+        if "CHUNSOFT" in portrait_authors: portrait_authors = ["CHUNSOFT"]
+        portraits.append([portrait_name,portrait_authors])
+
+    return portraits
+
+#Get attributions for sprites of a certain mon-form combination
+def get_sprite_attributions(driver,pokemon_name="",pokemon_form_name=""): #Don't call this on its own.
+    ignored_exceptions=(NoSuchElementException,StaleElementReferenceException,)
+    sprites_path = "//div[contains(concat(' ', @class, ' '), ' MuiContainer-root ')]/div[2]/div[2]"
+    assert driver.find_element(By.XPATH,sprites_path+"/div/div/h5").get_attribute("innerHTML") == "Sprites", "Pathing to sprites failed."
+    try:
+        driver.find_element(By.XPATH,sprites_path+"/h6")
+        print(f"No sprites in Repo for {pokemon_name} {pokemon_form_name}.",file=sys.stderr)
+        return ''
+    except NoSuchElementException:
+        #We don't actually need to do anything here because if this element doesn't exist, good.
+        pass
+    sprite_objects = driver.find_elements(By.XPATH, sprites_path+"/div[2]/div[contains(concat(' ', @class, ' '), ' MuiGrid-item ')]/div/div/div[2]")
+
+    sprites = []
+    for sprite in sprite_objects:
+        sprite_name = sprite.find_element(By.XPATH, ".//p").get_attribute("innerHTML")
+        sprite_hover = sprite.find_element(By.XPATH, ".//*[name()='svg']")
+        scroll_shim(driver,sprite_hover) #Fix MoveTargetOutOfBoundsException
+        ActionChains(driver).move_to_element(sprite_hover).perform()
+        popup = WebDriverWait(driver,10,ignored_exceptions=IGNORED_EXCEPTIONS).until(EC.presence_of_element_located((By.XPATH,"//div[contains(concat(' ', @class, ' '), ' MuiTooltip-popper ')]/div")))
+        sprite_authors = [el.get_attribute("innerHTML") for el in popup.find_elements(By.XPATH,".//div/div/a")]
+        if "CHUNSOFT" in sprite_authors: sprite_authors = ["CHUNSOFT"]
+        sprites.append([sprite_name,sprite_authors])
+
+    return sprites
+
+#Get all attributions for a certain mon-form combination.
+#Mon should be specified by pokedex number, form by name in lowercase.
+def get_pokemon_attributions_from_dex_number_and_form(driver,mon_number,form): #Call this.
+    driver.get(f"https://sprites.pmdcollab.org/#/{mon_number}")
+    assert 'PMD Sprite Repository' in driver.title
+
+    try:
+        driver.find_element(By.XPATH,"//div[@id='root']/div/h1")
+        print(f"Pokemon with dex number {mon_number} does not exist in repo.",file=sys.stderr)
+        return ''
+    except NoSuchElementException:
+        #We don't actually need to do anything here because if this element doesn't exist, good.
+        pass
+
+    header_path = "//div[contains(concat(' ', @class, ' '), ' MuiContainer-root ')]/div[1]/div[2]/div"
+    forms_button_path = header_path + "/div[contains(concat(' ', @class, ' '), ' MuiInputBase-root ')]"
+
+    #pokemon_name = WebDriverWait(driver, 10, ignored_exceptions=IGNORED_EXCEPTIONS).until(EC.presence_of_element_located((By.XPATH,header_path+"/h5"))).get_attribute("innerHTML")
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,forms_button_path))).click()
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH,"//div[@id='menu-']")))
+    pokemon_forms = [el.get_attribute("innerHTML") for el in driver.find_elements(By.XPATH,"//div[@id='menu-']/div[3]/ul/li/h6")]
+    driver.find_element(By.XPATH,f"//div[@id='menu-']/div[3]/ul/li/h6").click()
+
+    pokemon_forms = {k.lower():v for v,k in enumerate(pokemon_forms)}
+
+    try:
+        form_number = pokemon_forms[form]
+    except KeyError as e:
+        raise ValueError(f"Form {form} does not exist for pokemon with dex number {mon_number}!")
+    driver.get(f"https://sprites.pmdcollab.org/#/{mon_number}?form={form_number}")
+
+    pokemon_name = driver.find_element(By.XPATH,header_path+"/h5").get_attribute("innerHTML")
+    pokemon_form_name = driver.find_element(By.XPATH,forms_button_path+"/div/h6").get_attribute("innerHTML")
+    assert form == pokemon_form_name.lower(), "Pokemon form in repo not equivalent to entered Pokemon form."
+    assert mon_number in pokemon_name, "Pokedex number in repo not equivalent to entered Pokedex number."
+
+    portraits = get_portrait_attributions(driver,pokemon_name,pokemon_form_name)
+    sprites = get_sprite_attributions(driver,pokemon_name,pokemon_form_name)
+
+    portrait_attributions = []
+    for portrait_name,authors in portraits:
+        authors = ", ".join(set(authors))
+        portrait_attributions.append(f"{pokemon_name} {pokemon_form_name} {portrait_name} by {authors}, {"licensed under a proprietary license." if authors == "CHUNSOFT" else "licensed under CC-BY-NC-4.0."}")
+
+    sprite_attributions = []
+    for sprite_name,authors in sprites:
+        authors = ", ".join(set(authors))
+        sprite_attributions.append(f"{pokemon_name} {pokemon_form_name} {sprite_name} by {authors}, {"licensed under a proprietary license." if authors == "CHUNSOFT" else "licensed under CC-BY-NC-4.0."}")
+
+    return "\n".join(portrait_attributions+sprite_attributions)
+
+if __name__ == "__main__":
+    try:
+        os.environ['MOZ_HEADLESS'] = '1'
+        driver = webdriver.Firefox()
+
+        with open("input.txt",encoding="utf-8") as file:
+            mons = [(l[0].rjust(4,"0"),'normal' if len(l)==1 else l[1].lower().strip()) for l in [l.split(",") for l in file.read().splitlines()]]
+        
+        attributions = ""
+        for mon, form in tqdm(mons):
+            attributions += get_pokemon_attributions_from_dex_number_and_form(driver,mon,form) + "\n"
+        
+        with open("output.txt",mode="w",encoding="utf-8") as file:
+            file.write(attributions)
+        driver.quit()
+    except Exception as e:
+        driver.quit()
+        raise e
---
+++ readme.md
@@ -0,0 +1,34 @@
+This is a small program to properly attribute portraits and sprites from the [PMD Sprite Repo](https://sprites.pmdcollab.org/#/), in accordance with the Rules and Bylaws of [Creative Commons Attribution-NonCommercial 4.0 International](https://creativecommons.org/licenses/by-nc/4.0/?ref=chooser-v1).
+
+## Setup:
+
+- Requires [Python >=3.12](https://www.python.org/downloads/)
+  - You MUST check the option to "Add Python on PATH".
+- Requires [Firefox](https://www.mozilla.org/en-US/firefox/)
+- Requires [Gecko Driver](https://github.com/mozilla/geckodriver/releases) for Selenium, which should be placed in a folder along your PATH.
+  - If you don't know what that means, put it in the same folder as your python installation, which for Windows, defaults to `%localappdata%\Programs\Python\Python312`.
+- Then download these files on your computer and put them somewhere convenient.
+- Either double-click `setup.bat`, or in a terminal window run the command `py -m pip install selenium tqdm`.
+
+## Usage:
+Create, in the same folder as the program, a text file called `input.txt`.  
+In `input.txt`, add the National Dex numbers of each Pokémon you wish to have attributed from the [PMD Sprite Repo](https://sprites.pmdcollab.org/#/).  
+If you want to have attributed a form for a Pokémon that is not "Normal", add the form's NAME on the same line as the Natdex number, after a comma.
+
+Leading zeros do not matter in the Natdex number, nor does capitalization matter for the form name.
+
+I want the attributions for Beedrill, thus my `input.txt` would have:
+```
+15
+```
+I want the attributions for Starter Shiny Machop, and Female Sneasel. My `input.txt` would have:
+```
+066,starter shiny
+0215, Female
+```
+
+Double-click the file `main.py` to run it, or enter the command `py .\main.py` in a terminal window opened to the folder holding the program.  
+Wait for it to complete. Afterwards, fully-qualified attributions for each portrait and sprite of the Pokémon you listed will be in the file `output.txt` in the folder holding the program.
+
+## Troubleshooting:
+This hasn't been exhastively tested, and I wrote it over something like 8 hours while mildly sleep deprived. It might break.
